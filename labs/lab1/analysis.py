dbc525ce20473dc010dd3bda2b84538c3ec4037a
adengz <adengz@gmail.com>
2015-04-20 08:20:03 -0700

    add enthalpy calculator

--- labs/lab1/analysis.py
+++ labs/lab1/analysis.py
@@ -49,13 +49,68 @@ class NWChemAnalyzer(object):
                                            "Basis Set":basis_set}})
         return input_parameters
 
-    def _get_final_geometry(self, molecule):
-        output = self._output_dict[molecule]
+    def _get_final_geometry(self, mol_name):
+        output = self._output_dict[mol_name]
         mol = output.data[0]["molecules"][-1]
         return mol
 
+    @property
+    def final_geometry_parameters(self):
+        final_mol = {}
+        for mol_name in self._output_dict.keys():
+            final_mol.update({mol_name:
+                                  self._get_final_geometry(mol_name)})
+        h2 = final_mol["H2"]
+        h_h = h2.get_distance(0,1)
+        n2 = final_mol["N2"]
+        n_n = n2.get_distance(0,1)
+        nh3 = final_mol["NH3"]
+        n_h = 0
+        for i in range(1,4):
+            n_h += nh3.get_distance(0,i)
+        n_h = n_h/3
+        bond_lengths = {"H-H":h_h,"N-N":n_n,"N-H":n_h}
+        h_n_h = 0
+        for i,j in ((1,2),(2,3),(3,1)):
+            h_n_h +=nh3.get_angle(i,0,j)
+        h_n_h = h_n_h/3
+        geometry_parameters = {"Bond Length":bond_lengths,
+                               "Bond Angle":{"H-N-H":h_n_h}}
+        return geometry_parameters
 
-    def _get_final_energy(self, molecule):
-        output = self._output_dict[molecule]
+    def _get_final_energy(self, mol_name):
+        output = self._output_dict[mol_name]
         energy = output.data[2]["energies"][-1]
         return energy
+
+    @property
+    def final_energies(self):
+        final_energies = {}
+        for mol_name in self._output_dict.keys():
+            final_energies.update(
+                {mol_name:self._get_final_energy(mol_name)})
+        return final_energies
+
+    def _get_thermal_correction(self, mol_name):
+        output = self._output_dict[mol_name]
+        corre = output.data[1]["corrections"]["Thermal correction to Enthalpy"]
+        corre = corre*96.4869*0.0433634
+        return corre
+
+    @property
+    def enthalpy_corrections(self):
+        enthalpy_corrections = {}
+        for mol_name in self._output_dict.keys():
+            enthalpy_corrections.update(
+                {mol_name:self._get_thermal_correction(mol_name)})
+        return enthalpy_corrections
+
+    def formation_enthalpy(self):
+        energy = self.final_energies
+        corre = self.enthalpy_corrections
+        enthalpy = {}
+        for mol_name in self._output_dict.keys():
+            enthalpy.update({mol_name:energy[mol_name+corre[mol_name]]})
+        formation_enthalpy = enthalpy["NH3"]-(enthalpy["H2"]+enthalpy["N2"])
+        return formation_enthalpy
+
