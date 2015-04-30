6b984514731c170866e39b78c66668dbf6633043
adengz <adengz@gmail.com>
2015-04-30 15:48:57 -0700

    add volume parameter

--- labs/lab2/ulti_analyzer.py
+++ labs/lab2/ulti_analyzer.py
@@ -5,15 +5,17 @@ from pymatgen.util.plotting_utils import get_publication_quality_plot
 
 class UltiAnalyzer(object):
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, energy_scale='per_atom'):
         df = pd.read_csv(csv_file)
         self._summ_dict = {}
         for col_value in df.columns.values:
             self._summ_dict[col_value] = df[col_value].values
+        ef = {'per_atom':13.6057/2,'per_cell':13.6057}
         factor = {"ecut":13.6057, "alat":0.5292,
-                  "energy":13.6057/2, "total_force":13.6057/0.5292}
+                  "energy":ef[energy_scale], "total_force":13.6057/0.5292}
         for k,v in factor.items():
             self._summ_dict[k] *= v
+        self._summ_dict['volume'] = self._summ_dict['alat']**3/4.0
 
     @property
     def summ_dict(self):
