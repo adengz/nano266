a147fe2622af96721999efd37cbd2e75fb767009
adengz <adengz@gmail.com>
2015-05-14 10:02:37 -0700

    vastly change hcp input writer

--- labs/lab3/run_pw_hcp.py
+++ labs/lab3/run_pw_hcp.py
@@ -1,51 +1,21 @@
 #!/usr/bin/env python
 
-"""
-This is a very simple python starter script to automate a series of PWSCF
-calculations. If you don't know Python, get a quick primer from the official
-Python documentation at https://docs.python.org/2.7/. The script is deliberately
-simple so that only basic Python syntax is used and you can get comfortable with
-making changes and writing programs.
-
-Author: Shyue Ping Ong
-"""
 
 import os
-import shutil
+from monty.os import cd
 import numpy as np
 
-# Load the Si.pw.in.template file as a template.
-with open("Fe.hcp.pw.in.template") as f:
-    template = f.read()
+def write_hcp_input(alat,calat,k1=13,k3=8):
+    with open('Fe.hcp.pw.in.template') as f:
+        template = f.read()
+    s = template.format(alat=alat, calat=calat, k1=k1, k3=k3)
+    jobname = "Fe_hcp_%s_%s" % (calat,alat)
+    os.makedirs(jobname)
+    with open("%s/%s.pw.in" % (jobname,jobname), "w") as f:
+        f.write(s)
 
-# Set default values for various parameters
-#k = 8 # k-point grid of 8x8x8
-alat = 4.80 # The lattice parameter for the cell in Bohr.
-calat = 1.73
-k1 = 13
-k3 = 8
+if __name__ == "__main__":
+    with cd('scratch'):
+        for alat in np.linspace(4.7,4.9,21):
+            write_hcp_input(alat,1.71)
 
-# Loop through different k-points.
-for calat in np.linspace(1.71,1.74,4):
-    for alat in np.linspace(4.70,4.90,21):
-    # This generates a string from the template with the parameters replaced
-    # by the specified values.
-        s = template.format(alat=alat, calat=calat, k1=k1, k3=k3)
-
-    # Let's define an easy jobname.
-        jobname = "Fe_hcp_%s_%s" % (calat,alat)
-
-    # Write the actual input file for PWSCF.
-        with open("%s.pw.in" % jobname, "w") as f:
-            f.write(s)
-
-    #Print some status messages.
-        print("Running with alat = %s, k = %s..." % (alat, k1))
-    # Run PWSCF. Modify the pw.x command accordingly if needed.
-        os.system("pw.x -inp {jobname}.pw.in > {jobname}.out".format(jobname=jobname))
-
-        print("Done. Output file is %s.out." % jobname)
-
-# This just does cleanup. For this lab, we don't need the files that are
-# dumped into the tmp directory.
-shutil.rmtree("tmp")
