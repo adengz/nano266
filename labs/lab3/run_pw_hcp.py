b4550f80b5b3a37d3020e37bce730a97e1d62478
adengz <adengz@gmail.com>
2015-05-14 10:22:35 -0700

    add transfer psp

--- labs/lab3/run_pw_hcp.py
+++ labs/lab3/run_pw_hcp.py
@@ -2,6 +2,7 @@
 
 
 import os
+import shutil
 from monty.os import cd
 import numpy as np
 
@@ -13,9 +14,10 @@ def write_hcp_input(alat,calat,k1=13,k3=8):
     os.makedirs(jobname)
     with open("%s/%s.pw.in" % (jobname,jobname), "w") as f:
         f.write(s)
+    shutil.copyfile('../Fe.pbe-spn-kjpaw_psl.0.2.1.UPF',
+                    '%s/Fe.pbe-spn-kjpaw_psl.0.2.1.UPF' % jobname)
 
 if __name__ == "__main__":
     with cd('scratch'):
-        for alat in np.linspace(4.7,4.9,21):
-            write_hcp_input(alat,1.71)
+        write_hcp_input(4.8,1.71)
 
