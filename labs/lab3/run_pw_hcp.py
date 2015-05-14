f72735096d9adff5b7eec263336d607d23d5ed9f
adengz <adengz@gmail.com>
2015-05-14 10:05:27 -0700

    fix path for template

--- labs/lab3/run_pw_hcp.py
+++ labs/lab3/run_pw_hcp.py
@@ -6,7 +6,7 @@ from monty.os import cd
 import numpy as np
 
 def write_hcp_input(alat,calat,k1=13,k3=8):
-    with open('Fe.hcp.pw.in.template') as f:
+    with open('../Fe.hcp.pw.in.template') as f:
         template = f.read()
     s = template.format(alat=alat, calat=calat, k1=k1, k3=k3)
     jobname = "Fe_hcp_%s_%s" % (calat,alat)
