d32638d407e72a9a6ca1fc5e05b901ad18c311b6
adengz <adengz@gmail.com>
2015-05-14 22:05:02 -0700

    vastly changed UA with convergence test module

--- labs/lab3/ulti_analyzer.py
+++ labs/lab3/ulti_analyzer.py
@@ -2,48 +2,62 @@ __author__ = 'zhideng'
 
 import pandas as pd
 import numpy as np
-from scipy.interpolate import interp1d
 from pymatgen.util.plotting_utils import get_publication_quality_plot
 
 class UltiAnalyzer(object):
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, e_scale):
         df = pd.read_csv(csv_file)
         d = {}
         for col_value in df.columns.values:
             d[col_value] = df[col_value].values
         factor = {'ecut':13.6057, 'alat':0.5292,
-                  'energy':13605.7,
+                  'energy':13605.7*e_scale,
                   'total_force':13.6057/0.5292}
         for k,v in factor.items():
             d[k] *= v
-        #BCC
-        if d['calat'][0] == 0:
-            d['volume'] = d['alat']**3
-        #HCP
-        else:
-            d['energy'] *= 0.5
-            d['volume'] = 0.8660*d['calat']*d['alat']**3
         self._d = d
         self._df = df
+
+    def __getitem__(self, key):
+        return self._d[key]
 
     @property
     def dataframe(self):
         return self._df
 
-    @property
-    def summ_dict(self):
-        return self._d
+    def get_converged_kgrid(self, tol=1):
+        nkpts = self['nkpts']
+        assert nkpts.max() != nkpts.min()
+        e = self['energy']
+        filename = self['filename']
+        k = [int(f.split('.')[0].split('_')[2]) for f in filename]
+        self._d['kgrid'] = np.array(k)
+        ed = np.absolute(e[1:]-e[:-1])
+        if ed[-1] > tol:
+            raise ValueError('Not converged yet. Try larger k-point grid.')
+        else:
+            i = np.where(ed < tol)[0][0] + 1
+            return k[i]
 
-    def get_plot(self, xkey, ykey):
+    def get_kgrid_plot(self, tol=1):
+        ck = self.get_converged_kgrid(tol)
+        print 'Convergence reached at %d' % ck
         plt = get_publication_quality_plot(8,6)
-        plt.plot(self._d[xkey], self._d[ykey],
-                 'bo-',fillstyle='none')
+        k = self['kgrid']
+        e = self['energy']
+        plt.plot(k,e,'bo-',fillstyle='none')
+        ax = plt.gca()
+        xmin,xmax = ax.get_xlim()
+        for b in [e[-1]-tol,e[-1]+tol]:
+            plt.plot([xmin,xmax],[b,b],'k--',linewidth=2)
+        plt.ylabel('Energy (meV/atom)')
+        plt.xticks(k)
         return plt
 
     def _get_best_alat_ind(self):
-        e = self._d['energy']
-        a = self._d['alat']
+        e = self['energy']
+        a = self['alat']
         assert a.max() != a.min()
         ind = int(np.where(e==e.min())[0])
         return ind
@@ -51,7 +65,7 @@ class UltiAnalyzer(object):
     @property
     def best_lat(self):
         i = self._get_best_alat_ind()
-        return self._d['alat'][i], self._d['calat'][i]
+        return self['alat'][i], self['calat'][i]
 
 
 
