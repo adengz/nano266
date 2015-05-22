0ea1357a478649ba688f6ead37ed47cd4d41790b
adengz <adengz@gmail.com>
2015-05-22 13:25:08 -0700

    input writer supports both parallel and serial mode

--- pyqe/qeinput.py
+++ pyqe/qeinput.py
@@ -6,22 +6,52 @@ import shutil
 from monty.os import cd
 from pymatgen.core.composition import Composition
 
-def write_input(template,params,xkeys,func='pbe'):
-    jobinfo = template.split('.')
-    c = Composition(jobinfo[0])
-    elements = [e.symbol for e in c.elements]
-    with open('../%s.pw.in.template' % template) as f:
+def _write_input_from_temp(template, jobname, params):
+    '''
+    Private method as the writer of QE input file.
+    '''
+    with open('%s.pw.in' % jobname, 'w') as f:
+        f.write(template.format(**params))
+
+def get_template(template):
+    with open('%s.pw.in.template' % template) as f:
         t = f.read()
-    for k in xkeys:
-        jobinfo.append(str(params[k]))
-    jobname = "_".join(jobinfo)
-    os.makedirs(jobname)
-    with cd(jobname):
-        with open("%s.pw.in" % jobname, "w") as f:
-            f.write(t.format(**params))
+    struc_info = template.split('.')
+    return t, struc_info
+
+def write_input(temp, params, xkeys, func='pbe', parr=True):
+    '''
+    Method to write QE input file in scratch dir from template.
+    :param temp (list): Returned from get_template() method.
+    :param params (dict): Dict of parameters to pass into template. Keys
+        are the strings within the placeholders in the template.
+    :param xkeys ([str]): Parameters to distinguish between different
+        inpur files.temwr
+    :param func (str): Functional used. Default to GGA. Refer to the
+        format of filename of template files.
+    :param parr (bool): Whether use parallel mode (send the calculation
+        to computational nodes). Default to True. Note in non-parallel
+        (serial, run the calculation on login nodes), you need to cp the
+        psp files into scratch dir.
+    :return: None
+    '''
+    t, info = temp
+    pl = [str(params[k]) for k in xkeys]
+    jobname = "_".join(info+pl)
+    if parr:
+        c = Composition(info[0])
+        elements = [e.symbol for e in c.elements]
+        psp = {}
         for e in elements:
-            psp = glob.glob('../../%s.%s*.UPF' % (e,func))[0]
-            shutil.copyfile(psp,os.path.split(psp)[1])
+            psp[e] = os.path.abspath(glob.glob('../%s.%s*.UPF' % (e, func))[0])
+        os.makedirs(jobname)
+        with cd(jobname):
+            _write_input_from_temp(t, jobname, params)
+            for e in psp.keys():
+                shutil.copyfile(psp[e],os.path.split(psp[e])[1])
+    else:
+        _write_input_from_temp(t, jobname, params)
+
 
 if __name__ == '__main__':
     pass
