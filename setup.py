60df4f11843e11440b2dd3166fdd1accb23268d0
adengz <adengz@gmail.com>
2015-05-21 22:19:16 -0700

    add scripts to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-__author__ = 'zhideng'
+__author__ = 'Zhi Deng'
 
+import os
 from setuptools import setup, find_packages
 
+module_dir = os.path.dirname(os.path.abspath(__file__))
 setup(
     name = "pyqe",
     version = "0.1",
-    packages = find_packages()
+    packages = find_packages(),
+    scripts = [os.path.join('scripts', f) for f in
+               os.listdir(os.path.join(module_dir, 'scripts'))]
 )
