0ad4825566b3c4fe3d64edb7e42c90bd597efbb1
adengz <adengz@gmail.com>
2015-05-23 00:20:58 -0700

    add source code url on report

--- labs/lab2/analyze.py
+++ labs/lab2/analyze.py
@@ -13,8 +13,7 @@ patterns = {
     "ecut": re.compile("kinetic\-energy cutoff\s+=\s+([\d\.\-]+)\s+Ry"),
     "alat": re.compile("celldm\(1\)=\s+([\d\.]+)\s"),
     "nkpts": re.compile("number of k points=\s+([\d]+)"),
-    "total_force": re.compile("Total force =\s+([\d\.]+)"),
-    "cpu_time": re.compile("total cpu time spent up to now is\s+([\d\.\-]+)\s+secs")
+    "total_force": re.compile("Total force =\s+([\d\.]+)")
 }
 
 
@@ -31,7 +30,7 @@ def get_results(filename):
 
 
 def analyze(filenames):
-    fieldnames = ['filename', 'ecut', 'nkpts', 'alat', 'energy','total_force','cpu_time']
+    fieldnames = ['filename', 'ecut', 'nkpts', 'alat', 'energy','total_force']
     with open('results.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
