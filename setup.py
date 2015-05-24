0e700e407fa415d5bd225c544a6beb81dbfd7413
adengz <adengz@gmail.com>
2015-05-23 21:40:13 -0700

    add dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name = "pyqe",
     version = "0.1",
     packages = find_packages(),
+    install_requires=['numpy','pandas','monty','pymatgen'],
     scripts = [os.path.join('scripts', f) for f in
                os.listdir(os.path.join(module_dir, 'scripts'))]
 )
