453d7638b1c9900c149975d6308e69840252fe90
adengz <adengz@gmail.com>
2015-05-28 10:01:28 -0700

    add surface energy analysis module

--- pyqe/qe_surfio.py
+++ pyqe/qe_surfio.py
@@ -7,11 +7,11 @@ SURF = {'100':{'coords':np.array([[0.0, 0.0, 0.0],
                                   [0.5, 0.5, 0.0],
                                   [0.5, 0.0, 0.5],
                                   [0.0, 0.5, 0.5]]),
-               'a':1.0, 'c':1.0},
+               'a':1.0, 'c':1.0, 'area':1.0},
         '111':{'coords':np.array([[0.0, 0.0, 0.0],
                                   [0.666667, 0.333333, 0.333333],
                                   [0.333333, 0.666667, 0.666667]]),
-               'a':1/sqrt(2), 'c':sqrt(6)}}
+               'a':1/sqrt(2), 'c':sqrt(6)}, 'area':sqrt(3)/2}
 
 
 def get_slab_params(a0, miller_indices, nslab, nvac):
@@ -29,7 +29,7 @@ def get_slab_params(a0, miller_indices, nslab, nvac):
     nlayers = float(nslab + nvac)
     info = SURF[miller_indices]
     coords = info['coords'].copy()
-    alat = a0 * info['a']
+    alat = round(a0 * info['a'], 3)
     calat = nlayers * info['c']
     coords[:, 2] = coords[:, 2] / nlayers
     pos = []
@@ -44,3 +44,28 @@ def get_slab_params(a0, miller_indices, nslab, nvac):
     p = {'alat': alat, 'calat': calat, 'nslab': nslab, 'nvac': nvac,
          'k': 16, 'atompos': atompos, 'nat': nat, 'conv_thr': conv_thr}
     return p
+
+def analyze_surf(analyzer, miller_indices, e_bulk):
+    '''
+    Update nslab, nvac, natom and surf_energy to the pd.DataFrame.
+    :param analyzer: BasicAnalyzer object.
+    :param miller_indices (str): Miller indices. Only '100' and '111' are
+        supported.
+    :param e_bulk (float): Bulk energy in meV/atom.
+    :return: pd.df with surface related information
+    '''
+    df = analyzer.df
+    filename = analyzer['filename']
+    m = filename[0].split('_')[1]
+    assert miller_indices == m
+    info = SURF[miller_indices]
+    natom = len(info['coords'])
+    nslab = [int(f.split('_')[-2]) for f in filename]
+    nvac = [int(f.split('_')[-1]) for f in filename]
+    df['nslab'] = nslab
+    df['nvac'] = nvac
+    df['natom'] = natom * df['nslab']
+    area = analyzer['alat'][0] ** 2 * info['area']
+    df['surf_energy'] = 0.01602 / (2 * area) * \
+                        (df['energy'] - df['natom'] * e_bulk)
+    return df
