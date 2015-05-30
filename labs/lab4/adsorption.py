56241d6f05f2bad391f51d084eecc2c18fc4bb76
adengz <adengz@gmail.com>
2015-05-29 18:19:43 -0700

    improve init guess for adsor

--- labs/lab4/adsorption.py
+++ labs/lab4/adsorption.py
@@ -4,10 +4,10 @@ from pyqe.qe_input import get_template, write_input
 from monty.os import cd
 
 
-H_sites = {'top': (0, 0),
-           'bridge': (0.25, 0.25),
-           'hollow1': (0.33333, 0.16667),
-           'hollow2': (0.16667, 0.33333)}
+H_sites = {'top': (0, 0, 0.05),
+           'bridge': (0.25, 0.25, 0.04),
+           'hollow1': (0.33333, 0.16667, 0.03),
+           'hollow2': (0.16667, 0.33333, 0.03)}
 
 temp = get_template('AlH.111.adsor')
 with cd('scratch'):
